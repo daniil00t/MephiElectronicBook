8b0f770c2188a9e1daea04b57558a9887d59f101
daniil00t <mr.shenyagin.1@gmail.com>
2021-04-01 17:49:05 +0300

    fixed redirect from schedule page to report page; fixed styles report part 1

--- client/src/Components/PersonPage/Report/ItemTable.jsx
+++ client/src/Components/PersonPage/Report/ItemTable.jsx
@@ -9,17 +9,41 @@ class ItemTable extends Component {
 			activeRow: this.props.row,
 			activeCol: this.props.col,  
 			activeState: false,
-			value: this.props.value || "" 
+			value: this.props.value
 		};
 		this.textInput = React.createRef();
    }
 	activeItemTable(e){
-		if(!this.props.repedit.isEdit){
-			this.setState({
-				activeState: true
-			});
-			this.props.activate()
-			
+		if(this.props.col > 3){
+			if(this.props.typeReport != "att"){
+				if(!this.props.repedit.isEdit){
+					this.setState({
+						activeState: true
+					});
+					this.props.activate()
+				}
+			}
+			else{
+				if(this.state.value == "+" || this.state.value == "-"){
+					this.setState({
+						value: this.state.value == "-" ? "+" : "-",
+					});
+					this.props.addChange({
+						col: this.state.activeCol,
+						row: this.state.activeRow,
+						value: this.state.value == "-" ? "+" : "-"
+					})
+				}else{
+					this.setState({
+						value: "+",
+					});
+					this.props.addChange({
+						col: this.state.activeCol,
+						row: this.state.activeRow,
+						value: "+"
+					})
+				}
+			}
 		}
 	}
 	componentDidMount(){
@@ -54,16 +78,29 @@ class ItemTable extends Component {
 		this.props.activate()
 	}
    render() {
-		if(!this.state.activeState){
-			return (<td onClick={this.activeItemTable.bind(this)}><span className="item-data">{this.state.value}</span></td>);
-		}else{
-			return (<td><input ref={this.textInput} onKeyDown={this.pressKey.bind(this)} type="text" defaultValue={this.state.value} className="item-data"/><button onClick={this.exitLabel.bind(this)}>X</button></td>);
+		switch(this.props.typeReport){
+			case "att":
+				return (<td className="main" style={this.props.col == 3 ? {width: "320px", display: "block", height: "100%"} : {width: "50px"}} onClick={this.activeItemTable.bind(this)}><span className="item-data">{this.state.value}</span></td>);
+			case "score":
+				if(!this.state.activeState){
+					return (<td className="main" style={this.props.col == 3 ? {width: "320px", display: "block", height: "100%"} : {width: "50px"}} onClick={this.activeItemTable.bind(this)}><span className="item-data">{this.state.value}</span></td>);
+				}else{
+					return (<td className="main" style={{width: "50px", padding: 0, color: "red"}}><input autoFocus ref={input => this.textInput = input} type="number" onKeyDown={this.pressKey.bind(this)} defaultValue={this.state.value} className="item-data"/><button onClick={this.exitLabel.bind(this)}>X</button></td>);
+				}
+			case "ch":
+				if(!this.state.activeState){
+					return (<td className="main" style={this.props.col == 1 ? {width: "320px", display: "block", height: "100%"} : {width: "50px"}} onClick={this.activeItemTable.bind(this)}><span className="item-data">{this.state.value}</span></td>);
+				}else{
+					return (<td className="main" style={{width: "50px", padding: 0, color: "red"}}><input ref={this.textInput} type="number" onKeyDown={this.pressKey.bind(this)} defaultValue={this.state.value} className="item-data"/><button onClick={this.exitLabel.bind(this)}>X</button></td>);
+				}
 		}
+		
    }
 }
 
 const mapStateToProps = state => ({
-	repedit: state.report.edit
+	repedit: state.report.edit,
+	typeReport: state.report.typeReport
 })
 const mapDispatchToProps = dispatch => ({
 	activate: () => dispatch(changeStateEdit()),
